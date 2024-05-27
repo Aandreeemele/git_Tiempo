dc918801274ad13d2a79ccad8a9485860fdcb52e
Aandreemelele <aamelendez@scl.edu.gt>
2024-05-27 09:34:42 -0600

    cosas que toque

--- header/header.js
+++ header/header.js
@@ -1,1 +1,8 @@
-.
+let Santiago = document.querySelector(".header");
+
+Santiago.innerHTML=`
+<header class="santi">
+<p class="p5">Santa Catalina Loboure,  editor Jose Calderón</p>
+
+</header>
+`;
